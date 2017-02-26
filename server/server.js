57dbe52f4de1456f31da93b53c1634693aa22a68
Charles Newey <charlie@assemblyco.de>
2017-02-26 20:49:31 +0000

    Fix formatting, make consistent

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-//Author: Ramine Tinati
-//Purpose: Node server for the Panoptes Researcher Dashboard
-
+/*
+ * Author: Ramine Tinati
+ * Purpose: Node server for Panoptes Researcher Dashboard
+ */
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
@@ -9,346 +10,254 @@ var dateFormat = require('dateformat');
 var mongoose = require('mongoose');
 var Pusher = require('pusher-client');
 
-
-//-------------------
-//This is the working port
-//PLEASE NOTE: You must configure this in order for this to correctly run. 
-//Please find the port associate with your group and configure
+/*
+ * Server listening port. This must be configured correctly.
+ * Find the port associated with your group and configure appropriately.
+ */
 app.listen(3005);
 
-
-
-
-//-----------------------------
-//SECTION: Databases and models
-//We need to connect ot each of the datasets that we're going to be using.
-//if you have more, follow the same pattern to connect
-//NOTE: If you are running this locally, then the address for these datasets needs to be:
-//  mongodb://sotonwo.cloudapp.net/
-
-//Panoptes DATA
-var pm_con = mongoose.createConnection('mongodb://woUser:webobservatory@localhost/zoo_panoptes');
-var db_pm = pm_con;
+/*
+ * Define connections to databases and models here.
+ * If this is being run locally, the address for datasets must be:
+ * var mongo_url = 'mongodb://sotonwo.cloudapp.net/';
+ */
+var mongo_url = 'mongodb://woUser:webobservatory@localhost/zoo_panoptes';
+var db_pm = mongoose.createConnection(mongo_url);
 db_pm.on('error', console.error.bind(console, 'connection error:'));
 db_pm.once('open', function (callback) {
-    console.log("connected to database zoo_panoptes");
+  console.log('connected to database zoo_panoptes');
 });
 
-
-
-
-// //INFO: Mongoose requires that for a database connection, we create a schema, 
-// //INFO: this is then attached to a collection. 
-// //INFO: Three Schemas (but could use one if you wanted)
+/*
+ * Mongoose requires a schema for a database connection. This is then attached
+ * to a collection.
+ */
 var pmDoc = new mongoose.Schema({
   source: String,
   status: String,
 });
 
+/*
+ * Link the schemas to the collections.
+ */
+var pm_Model = db_pm.model('classifications', pmDoc);
+var pm_Model_Talk = db_pm.model('talk', pmDoc);
 
-// //INFO: These are the collections and models linked together
- var pm_Model = pm_con.model('classifications', pmDoc); 
-
-
- var pm_Model_Talk = pm_con.model('talk', pmDoc);
-
-// // var air_pollution_Model = air_pollution_con.model('tweets_20151100', twitterDoc_pol); 
-// // var shenzhen_Model = shenzhen_con.model('tweets_20151100', twitterDoc); 
-
-
-
-
-//----------------------------
-//SECTION: Pusher Work
-//INFO: Connec using the pusher-client object, then bind to a channel and emit the stream
-var socket = new Pusher('79e8e05ea522377ba6db',{
+/*
+ * Pusher
+ * Connect using pusher-client object, then bind to channel and emit stream.
+ */
+var socket = new Pusher('79e8e05ea522377ba6db', {
   encrypted: true
 });
-var channel = socket.subscribe('panoptes');
 
-//console.log("trying to get data from pusher...")
+var channel = socket.subscribe('panoptes');
 channel.bind('classification',
   function(data) {
-
-  //console.log(data)
-
     constructAndEmitLiveStream(data);
-
-  }
+  };
 );
-
 
 var channelTwo = socket.subscribe('talk');
-
 channelTwo.bind('comment',
   function(data) {
-
-    console.log(data)
-
     constructTalkAndEmitLiveStream(data);
-
-  }
+  };
 );
 
-
-
-
-// { classification_id: '10054109',
-//   project_id: '593',
-//   workflow_id: '338',
-//   user_id: '1447883',
-//   geo:
-//    { country_name: 'Greece',
-//      country_code: 'GR',
-//      city_name: 'Athens',
-//      coordinates: [ 23.7333, 37.9833 ],
-//      latitude: 37.9833,
-//      longitude: 23.7333 } }
-
-function constructAndEmitLiveStream(data){
-
-  //here we need to construct the object to confirm to what we were used to.
-
+/*
+ * Format for incoming data;
+ *
+ * {
+ *   classification_id: '10054109',
+ *   project_id: '593',
+ *   workflow_id: '338',
+ *   user_id: '1447883',
+ *   geo: {
+ *     country_name: 'Greece',
+ *     country_code: 'GR',
+ *     city_name: 'Athens',
+ *     coordinates: [ 23.7333, 37.9833 ],
+ *     latitude: 37.9833,
+ *     longitude: 23.7333
+ *   }
+ * }
+ */
+function constructAndEmitLiveStream(data) {
   var toSend = {};
+  try {
+    toSend['id'] = data.classification_id;
+    toSend['country_name'] = data.geo.country_name;
+    toSend['country_code'] = data.geo.country_code;
+    toSend['user_id'] = data.user_id
+    toSend['project_id'] = data.project_id
+    toSend['subjects'] = data.classification_id
+    toSend['created_at'] = new Date().toISOString()
+    toSend['lat'] = data.geo.coordinates[0]
+    toSend['lng'] = data.geo.coordinates[1]
 
-  try{
+    io.emit('panoptes_classifications', toSend)
 
-    toSend["id"] = data.classification_id;
-    toSend["country_name"] = data.geo.country_name;
-    toSend["country_code"] = data.geo.country_code;
-    toSend["user_id"] = data.user_id
-    toSend["project_id"] = data.project_id
-    toSend["subjects"] = data.classification_id
-    toSend["created_at"] = new Date().toISOString()
-    toSend["lat"] = data.geo.coordinates[0]
-    toSend["lng"] = data.geo.coordinates[1]
- 
-    //console.log("data")
-
-    io.emit("panoptes_classifications", toSend)
-
-    //also need to send this data to the database.
-    try{
+    try {
+      // send data to db
       saveData(toSend)
-      //console.log("saving data")
-    }catch(e1){
-        //console.log(e1)
+    } catch(e_inner) {
+      console.log(e_inner)
     }
-
-  }catch(e){
-
-      //console.log(e)
-	}	
-
-
-}
-
-function constructTalkAndEmitLiveStream(data){
-
-  //here we need to construct the object to confirm to what we were used to.
-
-  var toSend = {};
-
-  try{
-
-    toSend["id"] = data.project_id;
-    toSend["board_id"] = data.board_id;
-    toSend["discussion_id"] = data.discussion_id;
-    toSend["user_id"] = 0;
-    toSend["project_id"] = data.project_id;
-    toSend["section"] = data.section;
-    toSend["subject_id"] = data.focus_id;
-    toSend["created_at"] = new Date();
-    toSend["lat"] = 0;
-    toSend["lng"] = 0;
-
-    //console.log("data")
-
-    io.emit("panoptes_talk", toSend)
-
-    //also need to send this data to the database.
-    try{
-      saveDataTalk(toSend)
-      //console.log("saving data")
-    }catch(e1){
-        //console.log(e1)
-    }
-
-  }catch(e){
-
-      //console.log(e)
-
+  } catch(e_outer) {
+    console.log(e_outer)
   }
-
 }
 
+function constructTalkAndEmitLiveStream(data) {
+  var toSend = {};
+  try {
+    toSend['id'] = data.project_id;
+    toSend['board_id'] = data.board_id;
+    toSend['discussion_id'] = data.discussion_id;
+    toSend['user_id'] = 0;
+    toSend['project_id'] = data.project_id;
+    toSend['section'] = data.section;
+    toSend['subject_id'] = data.focus_id;
+    toSend['created_at'] = new Date();
+    toSend['lat'] = 0;
+    toSend['lng'] = 0;
 
+    io.emit('panoptes_talk', toSend)
+    try {
+      // send data to db
+      saveDataTalk(toSend)
+    } catch(e_inner) {
+      console.log(e_inner);
+    }
+  } catch(e_outer) {
+    console.log(e_outer)
+  }
+}
 
+function saveData(obj) {
+  try {
+    var doc = new pm_Model({
+      source: 'panoptes_zooniverse',
+      status: JSON.stringify(obj),
+    });
 
+    doc.save(function(err, doc) {
+      if (err) {
+        return console.error(err);
+      }
+    });
 
-
-function saveData(obj){
-
-    try{
-        
-        //console.log("Data: "+obj);
-
-        var doc = new pm_Model({
-            source: "panoptes_zooniverse",
-            status: JSON.stringify(obj),
-        });
-
-        doc.save(function(err, doc) {
-        if (err) return console.error(err);
-        //console.dir(thor);
-        });
-            
-            
-        //console.log("Added New Items: "+data);
-        //emit to real-time
-      return true;
-        }catch(e){
-          console.log(e)
-        }
-
+    return true;
+  } catch(e) {
+    console.log(e)
+  }
 }
 
 
 function saveDataTalk(obj){
+  try {
+    var doc = new pm_Model_Talk({
+      source: 'panoptes_zooniverse',
+      status: JSON.stringify(obj),
+    });
 
-    try{
+    doc.save(function(err, doc) {
+      if (err)  {
+        return console.error(err);
+      }
+    });
 
-        //console.log("Data: "+obj);
-
-        var doc = new pm_Model_Talk({
-            source: "panoptes_zooniverse",
-            status: JSON.stringify(obj),
-        });
-
-        doc.save(function(err, doc) {
-        if (err) return console.error(err);
-        //console.dir(thor);
-        });
-
-
-        //console.log("Added New Items: "+data);
-        //emit to real-time
-      return true;
-        }catch(e){
-          console.log(e)
-        }
-
+    return true;
+  } catch(e) {
+    console.log(e)
+  }
 }
 
 
-
-
-
-//----------------------------
-//SECTION: SOCKET IO work
-
-//INFO: When a connection is established with a client, the 'connection' port recieves a handshake
+/*
+ * Socket.IO
+ * When a connection is established with a client, the connection port receives
+ * a handshake.
+ */
 io.on('connection', function (socket) {
+  // we want to automatically load the data to the client
+  socket.on('load_data', function (data) {
+    console.log('Loading Map Data');
+    loadHistoricClassificationData(socket);
+  });
 
-     //we want to automatically load the data to the client
-     socket.on('load_data', function (data) {
-        console.log("Loading Map Data");
-        //console.log("emitting filter:", filter); 
-        loadHistoricClassificationData(socket);  
-    });
-
-    //  //we will then proceed to load the pollution data
-    //  socket.on('load_pollution_data', function (data) {
-    //     console.log("Socket load_pollution_data called");
-    //     //console.log("emitting filter:", filter); 
-    //     loadPollutionTweets(socket);      
-    // });
+  /*
+   * we will then proceed to load the pollution data...
+   *
+   * socket.on('load_pollution_data', function (data) {
+   *   console.log('Socket load_pollution_data called');
+   *   //console.log('emitting filter:', filter);
+   *   loadPollutionTweets(socket);
+   * });
+   */
 });
 
 
-//----------------------------
-//SECTION: Functions 
-
-//INFO: This function retrieves ALL the pollution data in the collection and streams it to the client
+/*
+ * Functions
+ * This function retrieves ALL the pollution data in the collection and streams
+ * it to the client.
+ */
 function loadHistoricClassificationData(socket){
-    console.log("Loading Historic Classification Data Timeseries");
+  console.log('Loading Historic Classification Data Timeseries');
 
-    var toSend = [];
-
-    var stream = pm_Model.find().stream();
-
-    stream.on('data', function (doc) {
-       var status = JSON.parse(doc.status);
-        //console.log(status.created_at);  
-        toSend.push(status.created_at);
-        //socket.emit("historic_data", toSend);
-        //toSend = [];
-      // do something with the mongoose document
-    }).on('error', function (err) {
-      // handle the error
-    }).on('close', function () {        
-      // the stream is closed
-
-        //pre-process timestamps and then send them
-        preprocesstimestamps(toSend,socket);
-
-       // socket.emit("finished_sending_historic_classification_data", "");
-        toSend = [];
-        //loadPM25Data(socket)
-    });
-
+  var toSend = [];
+  var stream = pm_Model.find().stream();
+  stream.on('data', function (doc) {
+    // do something with the mongoose document
+    var status = JSON.parse(doc.status);
+    toSend.push(status.created_at);
+  }).on('error', function (err) {
+    // handle error
+  }).on('close', function () {
+    // the stream is closed
+    // pre-process timestamps and then send them
+    preprocessTimestamps(toSend,socket);
+    toSend = [];
+  });
 }
 
-
-function preprocesstimestamps(toSend,socket){
-
- console.log("sending pre-processed timestamps as historic data")
+function preprocessTimestamps(toSend,socket){
+  console.log('sending pre-processed timestamps as historic data');
   var timestamp_dist = {};
   var timeseries = [];
 
-  for(var i=0; i<toSend.length; i++) {
-
+  for (var i = 0; i < toSend.length; i++) {
     var data = toSend[i];
     var tstamp = Date.parse(data)
-    var timestamp = dateFormat(tstamp, "yyyy-mm-dd hh:00:00");
+      var timestamp = dateFormat(tstamp, 'yyyy-mm-dd hh:00:00');
 
-    if(timestamp in timestamp_dist){
-        var cnt = timestamp_dist[timestamp];
-        timestamp_dist[timestamp] =cnt + 1
-    }else{
-        timestamp_dist[timestamp] = 1
+    if (timestamp in timestamp_dist) { // TODO: this is bad practice, fix
+      var cnt = timestamp_dist[timestamp];
+      timestamp_dist[timestamp] =cnt + 1
+    } else {
+      timestamp_dist[timestamp] = 1
     }
-
   }
-        socket.emit("historic_data", timestamp_dist);
 
-
+  socket.emit('historic_data', timestamp_dist);
 }
 
+/*
+ * General pattern for retrieving data and sending to client...
+ *   1. Client requests data using a socket pulse
+ *   2. Server queries database using stream, sends data to client via socket
+ *   3. When all data is sent, server notifies client with new socket pulse
+ *   4. Server then proceeds to next dataset.
+ * Currently this happens sequentially, but this is not necessary.
+ */
 
 
-//########################
-//NOTE: General patterns for retrieving data from the db and sending data to the client
-// 1. Client requests data using a socket pulse
-// 2. Server queries database using stream, sends data back to client via socket
-// 3. When all data is finished being sent, server notifies client with new socket pulse
-// 4. Server the proceeds to the next dataset.
-//NOTE: Currently this happens sequentially, but this is not necessary...!
-//#######################
-
-
-
-
-
-
-
-//INFO: Misc functions whihc we use for the HTTP server.
-function showErr (e) {
-    console.error(e, e.stack);
-}
-
+/*
+ * Miscellaneous HTTP functions
+ */
 function handler (req, res) {
-    res.writeHead(200);
-    res.end("");
+  res.writeHead(200);
+  res.end('');
 }
-
