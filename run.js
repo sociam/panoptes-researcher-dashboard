e875b0d11b771c0ec3bbc8dfbc88cf04c68a48dc
Charles Newey <charlie@assemblyco.de>
2017-03-22 22:17:43 +0000

    Add configuration variables to package.json

--- run.js
+++ run.js
@@ -1,37 +1,34 @@
 /**
  * Panoptes Researcher Dashboard run script
- * This (completely ugly and horrible) script
+ * This (compvarely ugly and horrible) script
  */
 var finalHandler = require('finalhandler');
 var http = require('http')
 var serveIndex = require('serve-index');
 var serveStatic = require('serve-static');
+var env = process.env;
 
-let BACKEND_HTTP_PORT = 3005;
-let FRONTEND_HTTP_PORT = 8080;
-let FRONTEND_PATH = __dirname.concat('/frontend');
-
-console.log(FRONTEND_PATH);
 
 // Create and kick off backend server
 console.log('Kicking off backend server...');
-let backend = require('./backend/server.js');
-backend.start(BACKEND_HTTP_PORT);
+var backend = require('./backend/server.js');
+backend.start(env.npm_package_config_backend_port);
 
 // Create and kick off front-end server
 console.log('Kicking off frontend server...');
-let opts = {'dotfiles': 'ignore'};
+var opts = {'dotfiles': 'ignore'};
 
-let _static = serveStatic(FRONTEND_PATH);
-let _index = serveIndex(FRONTEND_PATH);
-let srv = http.createServer(function onRequest(req, res) {
+var _static = serveStatic(env.npm_package_config_frontend_static_path);
+var _index = serveIndex(env.npm_package_config_frontend_static_path);
+
+var srv = http.createServer(function onRequest(req, res) {
   let done = finalHandler(req, res);
   _static(req, res, function onNext(err) {
     if (err) {
       console.log(err);
     }
-
     _index(req, res, done);
   });
 });
-srv.listen(FRONTEND_HTTP_PORT);
+
+srv.listen(env.npm_package_config_frontend_port);
